2e153113051c7eafb939cdddcf1b9b5429359a52
Semenov Valentine <semenov.vualya@gmail.com>
2023-12-24 22:29:27 +0300

    Sixth lab

--- src/js/TestComponent.js
+++ src/js/TestComponent.js
@@ -4,22 +4,67 @@ import $ from 'jquery';
 
 export default {
   name: 'TestComponent',
+  data() {
+    return {
+      formData: {
+        name: '',
+        course: '',
+        definition: '',
+        det: '',
+        series: ''
+      },
+      options: [
+        { value: 'first', label: 'Число' },
+        { value: 'second', label: 'Матрица' },
+        { value: 'third', label: 'Множество' },
+        { value: 'fourth', label: 'Последовательность' }
+      ]
+    };
+  },
   mounted(){
     this.validateTestForm();
     updatePageHistory();
   },
   methods: {
+    submitForm() {
+      if (this.validateForm()) {
+        alert('Форма заполнена корректно. Данные могут быть отправлены.');
+        this.resetForm();
+      }
+    },
+    validateForm() {
+      if (!this.formData.name || !this.formData.course || !this.formData.definition || !this.formData.det || !this.formData.series) {
+        alert('Пожалуйста, заполните все обязательные поля.');
+        return false;
+      }
+
+      if (!/\d/.test(this.formData.definition)) {
+        alert('Пожалуйста, введите целочисленное значение.');
+        return false;
+      }
+
+      return true;
+    },
+    resetForm() {
+      this.formData = {
+        name: '',
+        course: '',
+        definition: '',
+        det: '',
+        series: ''
+      };
+    },
     validateTestForm() {
       const form = $('.contactTest form');
       const formElements = form[0].elements;
-    
+
       form.on("submit", function (e) {
         e.preventDefault();
         let isFormValid = true;
-    
+
         for (let i = 0; i < formElements.length; i++) {
           const element = $(formElements[i]);
-    
+
           if (element.attr('type') === "text") {
             if (!element.val()) {
               isFormValid = false;
@@ -28,19 +73,19 @@ export default {
               break;
             }
           }
-    
+
           if (element.attr('type') === "radio") {
             const radioGroupName = element.attr('name');
             const radioGroup = $(`input[name="${radioGroupName}"]`);
             let isRadioGroupValid = false;
-    
+
             for (let j = 0; j < radioGroup.length; j++) {
               if (radioGroup[j].checked) {
                 isRadioGroupValid = true;
                 break;
               }
             }
-    
+
             if (!isRadioGroupValid) {
               isFormValid = false;
               alert("Пожалуйста, выберите ответ на вопрос.");
@@ -48,28 +93,19 @@ export default {
             }
           }
         }
-    
+
         const question1Input = $('#question1');
         const value = question1Input.val().trim();
         if (!/\d/.test(value)) {
           alert("Пожалуйста, введите целочисленное значение.");
           question1Input.focus();
         }
-    
+
         if (isFormValid) {
           alert("Форма заполнена корректно. Данные могут быть отправлены.");
           form[0].reset();
         }
       });
     }
-    
   }
-}
-
-
-
-
-
-
-
-
+};
